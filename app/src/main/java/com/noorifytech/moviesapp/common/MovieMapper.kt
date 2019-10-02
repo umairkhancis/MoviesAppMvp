a473ebad2929c77030a9792255fc853155ad4b9b
umair.khan <umair.khan@dubizzle.com>
2019-10-03 03:05:46 +0400

    Fix errors

--- app/src/main/java/com/noorifytech/moviesapp/common/MovieMapper.kt
+++ app/src/main/java/com/noorifytech/moviesapp/common/MovieMapper.kt
@@ -17,8 +17,63 @@ object MovieMapper {
         return MovieVO(
             movieEntity.id,
             movieEntity.title,
-            movieEntity.posterPath,
+            movieEntity.imageUrl,
             movieEntity.page
         )
     }
 }
+
+//object MovieMapper {
+//    fun toMovies(moviesResponse: MoviesListResponse): List<MovieEntity> {
+//        val movies = moviesResponse.results
+//        return movies.map { toMovieEntity(it) }
+//    }
+//
+//    private fun toMovieEntity(movieDto: MovieDto): MovieEntity {
+//        return MovieEntity(
+//            movieDto.id,
+//            movieDto.title,
+//            movieDto.getPosterFullPath(),
+//            movieDto.page
+//        )
+//    }
+//
+//    fun toMovieDetailEntity(movieDto: MovieDto): MovieDetailEntity {
+//        return MovieDetailEntity(
+//            movieDto.id,
+//            movieDto.title,
+//            movieDto.getPosterFullPath(),
+//            movieDto.overview,
+//            toTimeStamp(movieDto.releaseDate),
+//            movieDto.voteAverage
+//        )
+//    }
+//
+//    fun toMovieVO(movieEntity: MovieEntity): MovieVO {
+//        return MovieVO(
+//            movieEntity.id,
+//            movieEntity.title,
+//            movieEntity.imageUrl,
+//            movieEntity.page
+//        )
+//    }
+//
+//    fun toMovieDetailVO(movieDetailEntity: MovieDetailEntity): MovieDetailVO {
+//        return MovieDetailVO(
+//            movieDetailEntity.id,
+//            movieDetailEntity.title,
+//            movieDetailEntity.imageUrl,
+//            movieDetailEntity.overview,
+//            movieDetailEntity.releaseDate,
+//            movieDetailEntity.voteAverage
+//        )
+//    }
+//
+//    @SuppressLint("SimpleDateFormat")
+//    fun toTimeStamp(dateStr: String): Long {
+//        val formatter = SimpleDateFormat("yyyy-MM-dd")
+//        val d = formatter.parse(dateStr)
+//
+//        return d.time
+//    }
+//}

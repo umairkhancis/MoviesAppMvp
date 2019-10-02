3c7b5455685dcfd90bc3b795b49c8648ad4f84ff
umair.khan <umair.khan@dubizzle.com>
2019-10-03 03:02:38 +0400

    Add details api.

--- app/src/main/java/com/noorifytech/moviesapp/data/dao/backend/impl/retrofit/TMDBApi.kt
+++ app/src/main/java/com/noorifytech/moviesapp/data/dao/backend/impl/retrofit/TMDBApi.kt
@@ -1,10 +1,12 @@
 package com.noorifytech.moviesapp.data.dao.backend.impl.retrofit
 
 import com.noorifytech.moviesapp.common.AppConstants.Companion.API_KEY
+import com.noorifytech.moviesapp.data.dao.backend.dto.MovieDto
 import com.noorifytech.moviesapp.data.dao.backend.dto.MoviesListResponse
 import io.reactivex.Observable
 import retrofit2.Response
 import retrofit2.http.GET
+import retrofit2.http.Path
 import retrofit2.http.Query
 
 interface TMDBApi {
@@ -13,4 +15,10 @@ interface TMDBApi {
         @Query("language") language: String = "en-US",
         @Query("page") page: String = "1"
     ): Observable<Response<MoviesListResponse>>
+
+    @GET("movie/{movie_id}?api_key=$API_KEY")
+    fun getMovieDetails(
+        @Path("movie_id") movieId: Int,
+        @Query("language") language: String = "en-US"
+    ): Observable<Response<MovieDto>>
 }

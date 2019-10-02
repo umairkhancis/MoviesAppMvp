61bb9faf523ca0b31da8bcd6d5d9aaba6c3beac1
umair.khan <umair.khan@dubizzle.com>
2019-10-03 03:11:21 +0400

    Fix errors

--- app/src/main/java/com/noorifytech/moviesapp/data/dao/backend/MoviesBackendDao.kt
+++ app/src/main/java/com/noorifytech/moviesapp/data/dao/backend/MoviesBackendDao.kt
@@ -1,10 +1,11 @@
 package com.noorifytech.moviesapp.data.dao.backend
 
+import com.noorifytech.moviesapp.data.dao.backend.dto.ApiResponse
 import com.noorifytech.moviesapp.data.dao.backend.dto.MovieDto
 import com.noorifytech.moviesapp.data.dao.backend.dto.MoviesListResponse
 import io.reactivex.Observable
 
 interface MoviesBackendDao {
-    fun getPopularMovies(page: Int = 1): Observable<MoviesListResponse>
+    fun getPopularMovies(page: Int = 1): Observable<ApiResponse<MoviesListResponse>>
     fun getMovieDetails(movieId: Int): Observable<MovieDto>
 }

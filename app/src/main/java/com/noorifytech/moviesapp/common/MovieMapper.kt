317715d6b2e8020db691a9fc43fbc38fe13186ab
umair.khan <umair.khan@dubizzle.com>
2019-10-03 03:22:47 +0400

    Fix errors

--- app/src/main/java/com/noorifytech/moviesapp/common/MovieMapper.kt
+++ app/src/main/java/com/noorifytech/moviesapp/common/MovieMapper.kt
@@ -1,8 +1,13 @@
 package com.noorifytech.moviesapp.common
 
+import android.annotation.SuppressLint
+import com.noorifytech.moviesapp.data.dao.backend.dto.MovieDto
 import com.noorifytech.moviesapp.data.dao.backend.dto.MoviesListResponse
+import com.noorifytech.moviesapp.data.dao.db.entity.MovieDetailEntity
 import com.noorifytech.moviesapp.data.dao.db.entity.MovieEntity
+import com.noorifytech.moviesapp.data.repository.vo.MovieDetailVO
 import com.noorifytech.moviesapp.data.repository.vo.MovieVO
+import java.text.SimpleDateFormat
 
 object MovieMapper {
     fun toMovies(moviesResponse: MoviesListResponse): List<MovieEntity> {
@@ -21,59 +26,43 @@ object MovieMapper {
             movieEntity.page
         )
     }
-}
 
-//object MovieMapper {
-//    fun toMovies(moviesResponse: MoviesListResponse): List<MovieEntity> {
-//        val movies = moviesResponse.results
-//        return movies.map { toMovieEntity(it) }
-//    }
-//
-//    private fun toMovieEntity(movieDto: MovieDto): MovieEntity {
-//        return MovieEntity(
-//            movieDto.id,
-//            movieDto.title,
-//            movieDto.getPosterFullPath(),
-//            movieDto.page
-//        )
-//    }
-//
-//    fun toMovieDetailEntity(movieDto: MovieDto): MovieDetailEntity {
-//        return MovieDetailEntity(
-//            movieDto.id,
-//            movieDto.title,
-//            movieDto.getPosterFullPath(),
-//            movieDto.overview,
-//            toTimeStamp(movieDto.releaseDate),
-//            movieDto.voteAverage
-//        )
-//    }
-//
-//    fun toMovieVO(movieEntity: MovieEntity): MovieVO {
-//        return MovieVO(
-//            movieEntity.id,
-//            movieEntity.title,
-//            movieEntity.imageUrl,
-//            movieEntity.page
-//        )
-//    }
-//
-//    fun toMovieDetailVO(movieDetailEntity: MovieDetailEntity): MovieDetailVO {
-//        return MovieDetailVO(
-//            movieDetailEntity.id,
-//            movieDetailEntity.title,
-//            movieDetailEntity.imageUrl,
-//            movieDetailEntity.overview,
-//            movieDetailEntity.releaseDate,
-//            movieDetailEntity.voteAverage
-//        )
-//    }
-//
-//    @SuppressLint("SimpleDateFormat")
-//    fun toTimeStamp(dateStr: String): Long {
-//        val formatter = SimpleDateFormat("yyyy-MM-dd")
-//        val d = formatter.parse(dateStr)
-//
-//        return d.time
-//    }
-//}
+    private fun toMovieEntity(movieDto: MovieDto): MovieEntity {
+        return MovieEntity(
+            movieDto.id,
+            movieDto.title,
+            movieDto.getPosterFullPath(),
+            movieDto.page
+        )
+    }
+
+    fun toMovieDetailEntity(movieDto: MovieDto): MovieDetailEntity {
+        return MovieDetailEntity(
+            movieDto.id,
+            movieDto.title,
+            movieDto.getPosterFullPath(),
+            movieDto.overview,
+            toTimeStamp(movieDto.releaseDate),
+            movieDto.voteAverage
+        )
+    }
+
+    fun toMovieDetailVO(movieDetailEntity: MovieDetailEntity): MovieDetailVO {
+        return MovieDetailVO(
+            movieDetailEntity.id,
+            movieDetailEntity.title,
+            movieDetailEntity.imageUrl,
+            movieDetailEntity.overview,
+            movieDetailEntity.releaseDate,
+            movieDetailEntity.voteAverage
+        )
+    }
+
+    @SuppressLint("SimpleDateFormat")
+    fun toTimeStamp(dateStr: String): Long {
+        val formatter = SimpleDateFormat("yyyy-MM-dd")
+        val d = formatter.parse(dateStr)
+
+        return d.time
+    }
+}
--- app/src/main/java/com/noorifytech/moviesapp/data/repository/impl/MoviesRepositoryImpl.kt
+++ app/src/main/java/com/noorifytech/moviesapp/data/repository/impl/MoviesRepositoryImpl.kt
@@ -6,6 +6,7 @@ import androidx.paging.RxPagedListBuilder
 import com.noorifytech.moviesapp.common.MovieMapper
 import com.noorifytech.moviesapp.data.dao.backend.MoviesBackendDao
 import com.noorifytech.moviesapp.data.dao.db.MoviesDBDao
+import com.noorifytech.moviesapp.data.dao.db.entity.MovieDetailEntity
 import com.noorifytech.moviesapp.data.repository.MoviesRepository
 import com.noorifytech.moviesapp.data.repository.vo.MovieDetailVO
 import com.noorifytech.moviesapp.data.repository.vo.MovieVO
@@ -38,29 +39,25 @@ class MoviesRepositoryImpl(
         return rxPagedListBuilder.buildObservable()
     }
 
-    override fun getMovieDetails(movieId: Int): Observable<MovieDetailVO> {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
-    }
-
-    //    override fun getMovieDetails(movieId: Int): Observable<MovieDetailVO> =
-//        moviesDBDao.getMovieDetails(movieId)
-//            .flatMap { movieDetailEntity: MovieDetailEntity? ->
-//                if (movieDetailEntity != null) {
-//                    Observable.just(movieMapper.toMovieDetailVO(movieDetailEntity))
-//                } else {
-//                    moviesBackendDao.getMovieDetails(movieId)
-//                        .flatMap {
-//                            Observable.just(movieMapper.toMovieDetailEntity(it))
-//                        }
-//                        .flatMap {
-//                            moviesDBDao.insert(it)
-//                            Observable.just(it)
-//                        }
-//                        .flatMap {
-//                            Observable.just(movieMapper.toMovieDetailVO(it))
-//                        }
-//                }
-//            }
+    override fun getMovieDetails(movieId: Int): Observable<MovieDetailVO> =
+        moviesDBDao.getMovieDetails(movieId)
+            .flatMap { movieDetailEntity: MovieDetailEntity? ->
+                if (movieDetailEntity != null) {
+                    Observable.just(movieMapper.toMovieDetailVO(movieDetailEntity))
+                } else {
+                    moviesBackendDao.getMovieDetails(movieId)
+                        .flatMap {
+                            Observable.just(movieMapper.toMovieDetailEntity(it))
+                        }
+                        .flatMap {
+                            moviesDBDao.insert(it)
+                            Observable.just(it)
+                        }
+                        .flatMap {
+                            Observable.just(movieMapper.toMovieDetailVO(it))
+                        }
+                }
+            }
 
     companion object {
         const val PAGE_SIZE = 20
